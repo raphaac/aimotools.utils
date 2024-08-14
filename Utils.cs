d051653ba728d9fad0b6efe6bffcdf99eaf513b0
Raphael Correa <raphael.abrantes@trieengineering.com>
2024-08-14 12:15:29 -0300

    teste novo

---
+++ Utils.cs
@@ -0,0 +1,13 @@
+﻿namespace AimoTools.Utils
+{
+    public class Utils
+    {
+        public string GetRandomString(int length)
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                               .Select(s => s[random.Next(s.Length)]).ToArray());
+        }        
+    }
+}
