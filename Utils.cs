ec1cfae148b379d9fb6ac63425022b4400102816
Raphael A. Correa <rapha.ac@gmail.com>
2024-08-14 16:36:50 -0300

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -7,7 +7,7 @@
             var random = new Random();
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             return new string(Enumerable.Repeat(chars, length)
-                               .Select(s => s[random.Next(s.Length)]).ToArray());
+                               .Select(s => s[random.Next(s.Length)]).ToArray()); 
         }        
     }
 }
